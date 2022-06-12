5bcf773c8b90333d3951bdb37bbe773b189df848
debill <1pcpcpc1pc@gmail.com>
2022-06-12 17:21:35 +0300

    Pinentry mode option

--- xov.sh
+++ xov.sh
@@ -1,21 +1,23 @@
 #!/bin/sh
 
 name="xov"
+passmode="loopback"
 
 function help {
     echo $'
     -e [editor] : where to open,
+    -p : pop up a pinentry (window),
     -l(oad) : pull,
     -u(pload) : push\n'
     git status -s
 }
 
 function encrypt {
-    gpg --yes --armor --output $file --pinentry-mode=loopback --symmetric --cipher-algo AES256 ./.$name/$file
+    gpg --yes --armor --output $file --pinentry-mode=$passmode --symmetric --cipher-algo AES256 ./.$name/$file
 }
 
 function decrypt {
-    gpg --yes --armor --output ./.$name/$file --pinentry-mode=loopback --cipher-algo AES256 --decrypt $file
+    gpg --yes --armor --output ./.$name/$file --pinentry-mode=$passmode --cipher-algo AES256 --decrypt $file
 }
 
 function openfile {
@@ -42,10 +44,11 @@ function upload {
     git push origin HEAD:main
 }
 
-while getopts e:luh flag
+while getopts e:pluh flag
 do
     case ${flag} in 
     e) editor=${OPTARG};;
+    p) passmode="ask";;
     l) load; exit;;
     u) upload; exit;;
     h) help; exit;;
